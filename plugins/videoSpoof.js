b1a1d35d91924e0dd07a730f5164e1a446d2428f
Dwait Pandhi <66095573+gamerwaves@users.noreply.github.com>
2025-03-13 14:19:07 -0400

    Create videoSpoof.js

---
+++ plugins/videoSpoof.js
@@ -0,0 +1,21 @@
+const originalFetch = window.fetch;
+
+window.fetch = async function (input, init) {
+    let body;
+    if (input instanceof Request) body = await input.clone().text();
+    else if (init && init.body) body = init.body;
+    if (features.videoSpoof && body && body.includes('"operationName":"updateUserVideoProgress"')) {
+        try {
+            let bodyObj = JSON.parse(body);
+            if (bodyObj.variables && bodyObj.variables.input) {
+                const durationSeconds = bodyObj.variables.input.durationSeconds;
+                bodyObj.variables.input.secondsWatched = durationSeconds;
+                bodyObj.variables.input.lastSecondWatched = durationSeconds;
+                body = JSON.stringify(bodyObj);
+                if (input instanceof Request) { input = new Request(input, { body: body }); } 
+                else init.body = body; 
+            }
+        } catch (e) { debug(`🚨 Error @ videoSpoof.js\n${e}`); }
+    }
+    return originalFetch.apply(this, arguments);
+};
